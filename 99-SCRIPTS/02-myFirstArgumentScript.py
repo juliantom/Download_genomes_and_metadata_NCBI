5473e19e5df1c920a92c37a6da64e5f8fa78ba5a
juliantom <torresjulian125@gmail.com>
2023-02-27 14:06:42 -0500

    Updated 02/27/23 2pm

---
+++ 99-SCRIPTS/02-myFirstArgumentScript.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This is a awesome
+        python script!"""
+        
+def is_tool(name):
+    """Check whether `name` is on PATH and marked as executable."""
+
+    # from whichcraft import which
+    from shutil import which
+
+    return which(name) is not None
+
+which(wget)
