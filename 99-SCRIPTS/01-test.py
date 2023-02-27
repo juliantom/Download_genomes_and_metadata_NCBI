bb43eee3b0709d30a28df848f689f9c6b6dd7ebf
juliantom <torresjulian125@gmail.com>
2023-02-27 13:38:18 -0500

    Created 02/27/23 2pm

--- 99-SCRIPTS/01-test.py
+++ 99-SCRIPTS/01-test.py
@@ -4,13 +4,36 @@
 """This is a awesome
         python script!"""
 
+print ("")
+print ("START")
+print ("-------------------------")
 
 import argparse
+# Explanation message 
+msg = "A test program v2."
+parser = argparse.ArgumentParser(description = msg)
 
-parser = argparse.ArgumentParser(description='A test program.')
-
-parser.add_argument("-p", "--print_string", help="Prints the supplied argument.", default="A random string.")
+parser.add_argument("-i","--input", help="Input file.", default="First random string.", required=True)
+parser.add_argument("-m","--modifier", help="Conditional modifier.", required=False)
+parser.add_argument("-e","--external_file", help="External file. Required.", required=False)
+parser.add_argument("-v","--value", help="Value (integer)", required=False, type=int)
+parser.add_argument("-vf","--value_float", help="Value (float)", required=False, type=float)
+parser.add_argument("-o","--ouput", help="Output file.", required=True)
 
 args = parser.parse_args()
 
-print(args.print_string)
+# 
+print("Input file=",args.input)
+print("Modifier=",args.modifier)
+print("External data=",args.external_file)
+print("Value (integer)=",args.value)
+print("Value (float)=",args.value_float)
+print("Output file=",args.ouput)
+
+#
+print ("")
+config = vars(args)
+print(config)
+print ("")
+print ("END")
+print ("-------------------------")
