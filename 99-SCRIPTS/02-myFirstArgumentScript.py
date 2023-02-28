47a75a5074fe665fb0277d77c317333d681a6019
juliantom <torresjulian125@gmail.com>
2023-02-28 09:42:38 -0500

    Updated 02/28/2023

--- 99-SCRIPTS/02-myFirstArgumentScript.py
+++ 99-SCRIPTS/02-myFirstArgumentScript.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""This is a awesome
+"""This script 
         python script!"""
-        
+import argparse
+# Explanation message 
+msg = "This program checks if a program is present (in the PATH)"
+parser = argparse.ArgumentParser(description = msg)
+
+parser.add_argument("-p","--program", help="A program name.", required=True)
+
+args = parser.parse_args()
+
+
+# Function to check if command NCBI-datasets is in the PATH
 def is_tool(name):
     """Check whether `name` is on PATH and marked as executable."""
 
@@ -12,4 +22,14 @@ def is_tool(name):
 
     return which(name) is not None
 
-which(wget) 
+check_is_tool = is_tool(args.program)
+if check_is_tool == False:
+    check_is_tool = "NO"
+else:
+    check_is_tool = "YES"
+# 
+print("")
+print("Checking if required program(s) are in the $PATH...")
+print("------------------------------")
+print("Program", "'" + args.program + "'" + " found:", check_is_tool)
+####
